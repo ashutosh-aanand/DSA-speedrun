bb5b357aeffa3e741dbab733aef9fa0d4e8b3b98
ashutosh-aanand <ashutosh.aanand@gmail.com>
2020-11-09 10:56:15 +0530

    Pascal's triangle

---
+++ 1. Array/Pascal's triangle.cpp
@@ -0,0 +1,67 @@
+/*
+Given a non-negative integer numRows, generate the first numRows of
+Pascal's triangle.
+
+Example:
+
+Input: 5
+Output:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+typedef pair<int,int> pii;
+
+void fast_IO() { ios_base::sync_with_stdio(false);cin.tie(NULL);}
+void test_locally(){ freopen("in.txt","r",stdin);freopen("out.txt","w",stdout);}
+
+// m1)
+
+vector<vector<int> > m1(int n)
+{
+	vector<vector<int> > p(n);
+	p[0].push_back(1);
+	if(n==1) return p;
+
+	p[1].push_back(1);
+	p[1].push_back(1);
+	if(n==2) return p;
+
+	for(int i=2;i<n;i++)
+	{
+		p[i].push_back(1);
+		for(int j=1;j<p[i-1].size();j++)
+		{
+			p[i].push_back(p[i-1][j-1]+p[i-1][j]);
+		}
+		p[i].push_back(1);
+	}
+	return p;
+}
+
+
+int main()
+{
+	fast_IO();
+	test_locally();
+
+	//input
+	int n; cin>>n;
+
+	vector<vector<int> > ans = m1(n);
+	for(auto x: ans)
+	{
+		for(int y: x) cout<<y<<",";
+		cout<<"\n";
+	}
+}
