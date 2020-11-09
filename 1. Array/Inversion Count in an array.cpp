7c6d834755c73d92eba4545ffb74c134fd2b4c52
ashutosh-aanand <ashutosh.aanand@gmail.com>
2020-11-09 12:19:49 +0530

    Inversion count using Merge Sort Algo.

--- 1. Array/Inversion Count in an array.cpp
+++ 1. Array/Inversion Count in an array.cpp
@@ -47,6 +47,60 @@ void m1(vector<int> a, int n)
 	cout<<ans<<"\n";
 }
 
+// m2) using merge sort algorithm
+
+// TIme: O(nlog(n))
+// Space: O(n), due to temp array
+
+vector<int> temp;
+int merge(vector<int>& a, int l, int m, int r)
+{
+	int count=0;
+
+	int i=l, j=m+1;
+	while(i<=m && j<=r)
+	{
+		if(a[i]<=a[j])
+		{
+			temp.push_back(a[i]);
+			i++;
+		}
+		else{
+			temp.push_back(a[j]);
+			j++;
+			count+=(m-i+1);
+		}
+	}
+	while(i<=m) temp.push_back(a[i++]);
+	while(j<=r) temp.push_back(a[j++]);
+
+	int k=0;
+	for(i=l;i<=r;i++)
+	{
+		a[i]=temp[k];
+		k++;
+	}
+	temp.clear();
+
+	return count; 
+}
+
+int inversions(vector<int>& a, int l, int r)
+{
+	if(l==r) return 0;
+
+	int mid = l+(r-l)/2;
+
+	int inversions_left = inversions(a,l,mid);
+	int inversions_right= inversions(a,mid+1,r);
+
+	int inversions_merge = merge(a,l,mid,r);
+
+	return inversions_left + inversions_right + inversions_merge;
+}
+
+
+
 int main()
 {
 	fast_IO();
@@ -57,5 +111,6 @@ int main()
 	vector<int> a(n);
 	for(int i=0;i<n;i++) cin>>a[i];
 
-	m1(a,n);
+	// m1(a,n);
+	cout<<inversions(a,0,n-1);
 }
